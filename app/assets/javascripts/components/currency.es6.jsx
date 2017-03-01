3c8188208b882ddad9bd031ed54f5c63c1364593
ChesterHub <ChesterLHoang@gmail.com>
2017-03-01 01:14:44 -0800

    add values for options

--- app/assets/javascripts/components/currency.es6.jsx
+++ app/assets/javascripts/components/currency.es6.jsx
@@ -31,19 +31,20 @@ class Currency extends React.Component {
     }
 
   render () {
+    let cur = this.props.currency.Breach.ChaosOrb.armourer_scrap
     return (
     	<div>
-        <p>{this.props.currency.Breach.ChaosOrb.armourer_scrap}</p>
+        <p>{cur}</p>
     		<h3>Choose league:</h3>
     		<form value={this.state.league} onChange={this.changeLeague.bind(this)}>
     			<select>
                     <optgroup label="Current">
-    				<option value="standard">Standard</option>
-    				<option value="hardcore">Hardcore</option>
+    				<option value="Standard">Standard</option>
+    				<option value="Hardcore">Hardcore</option>
                     </optgroup>
                     <optgroup label="Old">
-    				<option value="breach">Breach</option>
-    				<option value="breach+hardcore">Hardcore Breach</option>
+    				<option value="Breach">Breach</option>
+    				<option value="Breach+Hardcore">Hardcore Breach</option>
                     </optgroup>
     			</select>
     		</form>
@@ -54,62 +55,62 @@ class Currency extends React.Component {
     				<select onChange={this.offerCurrency.bind(this)}>
                         <option selected="true" disabled="disabled"> -- Select Currency -- </option>
                         <optgroup label="Orbs">
-    					<option value="chaos">Chaos Orb</option>
-    					<option value="exalted">Exalted Orb</option>
-    					<option value="blessed">Blessed Orb</option>
-    					<option value="chrome">Chromatic Orb</option>
-    					<option value="divine">Divine Orb</option>
-    					<option value="jeweller">Jeweller's Orb</option>
-    					<option value="alchemy">Orb of Alchemy</option>
-    					<option value="alteration">Orb of Alteration</option>
-    					<option value="augmentation">Orb of Augmentation</option>
-    					<option value="chance">Orb of Chance</option>
-    					<option value="fusing">Orb of Fusing</option>
-    					<option value="regret">Orb of Regret</option>
-    					<option value="scouring">Orb of Scouring</option>
-    					<option value="transmute">Orb of Transmutation</option>
-    					<option value="regal">Regal Orb</option>
-    					<option value="vaal">Vaal Orb</option>
+    					<option value="ChaosOrb">Chaos Orb</option>
+    					<option value="ExaltedOrb">Exalted Orb</option>
+    					<option value="BlessedOrb">Blessed Orb</option>
+    					<option value="ChromaticOrb">Chromatic Orb</option>
+    					<option value="DivineOrb">Divine Orb</option>
+    					<option value="JewellerOrb">Jeweller's Orb</option>
+    					<option value="OrbofAlchemy">Orb of Alchemy</option>
+    					<option value="OrbofAlteration">Orb of Alteration</option>
+    					<option value="OrbofAugmentation">Orb of Augmentation</option>
+    					<option value="OrbofChance">Orb of Chance</option>
+    					<option value="OrbofFusing">Orb of Fusing</option>
+    					<option value="OrbofRegret">Orb of Regret</option>
+    					<option value="OrbofScouring">Orb of Scouring</option>
+    					<option value="OrbofTransmute">Orb of Transmutation</option>
+    					<option value="RegalOrb">Regal Orb</option>
+    					<option value="VaalOrb">Vaal Orb</option>
                         </optgroup>
                         <optgroup label="Other">
-                        <option value="armourer">Armourer's Scrap</option>
-                        <option value="blacksmith">Blackmsith's Whetstone</option>
-                        <option value="cartographer">Cartographer's Chisel</option>
-                        <option value="gemcutter">Gemcutter's Prism</option>
-                        <option value="glassblower">Glassblower's Bauble</option>
-                        <option value="portal">Portal Scroll</option>
-                        <option value="wisdom">Scroll of Wisdom</option>
+                        <option value="ArmourerScrap">Armourer's Scrap</option>
+                        <option value="BlacksmithWhetstone">Blackmsith's Whetstone</option>
+                        <option value="CartographerChisel">Cartographer's Chisel</option>
+                        <option value="GemcutterPrism">Gemcutter's Prism</option>
+                        <option value="GlassblowerBubble">Glassblower's Bauble</option>
+                        <option value="PortalScroll">Portal Scroll</option>
+                        <option value="ScrollofWisdom">Scroll of Wisdom</option>
                         </optgroup>
     				</select>
     			  : 
     			 	<select onChange={this.receiveCurrency.bind(this)}>
                         <option selected="true" disabled="disabled"> -- Select Currency -- </option>
                         <optgroup label="Orbs">
-    			 		<option value="chaos">Chaos Orb</option>
-    					<option value="exalted">Exalted Orb</option>
-    					<option value="blessed">Blessed Orb</option>
-    					<option value="chrome">Chromatic Orb</option>
-    					<option value="divine">Divine Orb</option>
-    					<option value="jeweller">Jeweller's Orb</option>
-    					<option value="alchemy">Orb of Alchemy</option>
-    					<option value="alteration">Orb of Alteration</option>
-    					<option value="augmentation">Orb of Augmentation</option>
-    					<option value="chance">Orb of Chance</option>
-    					<option value="fusing">Orb of Fusing</option>
-    					<option value="regret">Orb of Regret</option>
-    					<option value="scouring">Orb of Scouring</option>
-    					<option value="transmute">Orb of Transmutation</option>
-    					<option value="regal">Regal Orb</option>
-    					<option value="vaal">Vaal Orb</option>
+    			 		<option value="chaos_orb">Chaos Orb</option>
+    					<option value="exalted_orb">Exalted Orb</option>
+    					<option value="blessed_orb">Blessed Orb</option>
+    					<option value="chromatic_orb">Chromatic Orb</option>
+    					<option value="divine_orb">Divine Orb</option>
+    					<option value="jeweller_orb">Jeweller's Orb</option>
+    					<option value="orbof_alchemy">Orb of Alchemy</option>
+    					<option value="orbof_alteration">Orb of Alteration</option>
+    					<option value="orbof_augmentation">Orb of Augmentation</option>
+    					<option value="orbof_chance">Orb of Chance</option>
+    					<option value="orbof_fusing">Orb of Fusing</option>
+    					<option value="orbof_regret">Orb of Regret</option>
+    					<option value="orbof_scouring">Orb of Scouring</option>
+    					<option value="orbof_transmute">Orb of Transmutation</option>
+    					<option value="regal_orb">Regal Orb</option>
+    					<option value="vaal_orb">Vaal Orb</option>
                         </optgroup>
                         <optgroup label="Other">
-    					<option value="armourer">Armourer's Scrap</option>
-    					<option value="blacksmith">Blackmsith's Whetstone</option>
-    					<option value="cartographer">Cartographer's Chisel</option>
-    					<option value="gemcutter">Gemcutter's Prism</option>
-    					<option value="glassblower">Glassblower's Bauble</option>
-    					<option value="portal">Portal Scroll</option>
-    					<option value="wisdom">Scroll of Wisdom</option>
+    					<option value="armourer_scrap">Armourer's Scrap</option>
+    					<option value="blacksmith_whetstone">Blackmsith's Whetstone</option>
+    					<option value="cartographer_chisel">Cartographer's Chisel</option>
+    					<option value="gemcutter_prism">Gemcutter's Prism</option>
+    					<option value="glassblower_bubble">Glassblower's Bauble</option>
+    					<option value="portal_scroll">Portal Scroll</option>
+    					<option value="scrollof_wisdom">Scroll of Wisdom</option>
                         </optgroup>
     			 	</select>
     			<input type="submit" value="Search"/>
