dc82c2ad8a76638a34eb43e1e3c64159726c8c00
ChesterHub <ChesterLHoang@gmail.com>
2017-03-01 21:43:12 -0800

    More table editing

--- app/assets/javascripts/components/all_currency.es6.jsx
+++ app/assets/javascripts/components/all_currency.es6.jsx
@@ -2,10 +2,7 @@ class AllCurrency extends React.Component {
         constructor(props){
         super(props)
         this.state = {
-            league: "Standard",
-            currencyName: ""
-
-
+            league: "Breach"
         }
     }
 
@@ -16,10 +13,14 @@ class AllCurrency extends React.Component {
         for (let ratio in theOrb){
             if (theOrb.hasOwnProperty(ratio)) {
                 if (rowCounter === matchingInd){
-                    rows.push("-")
+                    rows.push(" -1- ")
                     rowCounter++
                 } else {
-                    rows.push(theOrb[ratio])
+                    if(theOrb[ratio] === 0){
+                        rows.push("Not Enough Data")
+                    } else {
+                        rows.push(theOrb[ratio])
+                    }
                     rowCounter++
                 }
 
@@ -33,127 +34,128 @@ class AllCurrency extends React.Component {
     return (
 
     	<div>
-    		<h3>General Currency Table</h3>
+    		<h3>Currency Table</h3>
+            <p>The Currency in row will give you the ratio of 1 to the Currency in the corresponding column.</p>
     		<table>
             <tbody>
     		<tr>
     			<th></th>
-    			<th>Blessed Orb</th>
-    			<th>Chaos Orb</th>
-    			<th>Chromatic Orb</th>
-    			<th>Divine Orb</th>
-    			<th>Exalted Orb</th>
-    			<th>Jeweller's Orb</th>
-    			<th>Orb of Alchemy</th>
-    			<th>Orb of Alteration</th>
-    			<th>Orb of Augmentation</th>
-    			<th>Orb of Chance</th>
-    			<th>Orb of Fusing</th>
-    			<th>Orb of Regret</th>
-    			<th>Orb of Scouring</th>
-    			<th>Orb of Transmutation</th>
-    			<th>Regal Orb</th>
-    			<th>Vaal Orb</th>
     			<th>Armourer's Scrap</th>
     			<th>Blackmith's Whetstone</th>
+                <th>Blessed Orb</th>
     			<th>Cartographer's Chisel</th>
+                <th>Chaos Orb</th>
+                <th>Chromatic Orb</th>
+                <th>Divine Orb</th>
+                <th>Exalted Orb</th>
     			<th>Gemcutter's Prism</th>
     			<th>Glassblower's Bauble</th>
-    			<th>Portal Scroll</th>
-    			<th>Scroll of Wisdom</th>
-    		</tr>
+                <th>Jeweller's Orb</th>
+                <th>Orb of Alchemy</th>
+                <th>Orb of Alteration</th>
+                <th>Orb of Augmentation</th>
+                <th>Orb of Chance</th>
+                <th>Orb of Fusing</th>
+                <th>Orb of Regret</th>
+                <th>Orb of Scouring</th>
+                <th>Orb of Transmutation</th>
+                <th>Portal Scroll</th>
+                <th>Regal Orb</th>
+                <th>Scroll of Wisdom</th>
+                <th>Vaal Orb</th>
+            </tr>
     		<tr>
-    			<th>Blessed Orb</th>
-                {this.addTD("BlessedOrb", 0).map((ratio) => <td>{ratio}</td>)}
-    		</tr>
-    		<tr>
-    			<th>Chaos Orb</th>
-    			{this.addTD("ChaosOrb", 1).map((ratio) => <td>{ratio}</td>)}
-    		</tr>
-    		<tr>
-    			<th>Chromatic Orb</th>
-    			{this.addTD("ChromaticOrb", 2).map((ratio) => <td>{ratio}</td>)}
-    		</tr>
-    		<tr>
-    			<th>Divine Orb</th>
-    			{this.addTD("DivineOrb", 3).map((ratio) => <td>{ratio}</td>)}
-    		</tr>
-    		<tr>
-    			<th>Exalted Orb</th>
-    			{this.addTD("ExaltedOrb", 4).map((ratio) => <td>{ratio}</td>)}
-    		</tr>
-    		<tr>
-    			<th>Jeweller's Orb</th>
-    			{this.addTD("JewellerOrb", 5).map((ratio) => <td>{ratio}</td>)}
-    		</tr>
-    		<tr>
-    			<th>Orb of Alchemy</th>
-    			{this.addTD("OrbofAlchemy", 6).map((ratio) => <td>{ratio}</td>)}
-    		</tr>
-    		<tr>
-    			<th>Orb of Alteration</th>
-    			{this.addTD("OrbofAlteration", 7).map((ratio) => <td>{ratio}</td>)}
-    		</tr>
-    		<tr>
-    			<th>Orb of Augmentation</th>
-    			{this.addTD("OrbofAugmentation", 8).map((ratio) => <td>{ratio}</td>)}
-    		</tr>
-    		<tr>
-    			<th>Orb of Chance</th>
-    			{this.addTD("OrbofChance", 9).map((ratio) => <td>{ratio}</td>)}
-    		</tr>
-    		<tr>
-    			<th>Orb of Fusing</th>
-    			{this.addTD("OrbofFusing", 10).map((ratio) => <td>{ratio}</td>)}
-    		</tr>
-    		<tr>
-    			<th>Orb of Regret</th>
-    			{this.addTD("OrbofRegret", 11).map((ratio) => <td>{ratio}</td>)}
-    		</tr>
-    		<tr>
-    			<th>Orb of Scouring</th>
-    			{this.addTD("OrbofScouring", 12).map((ratio) => <td>{ratio}</td>)}
-    		</tr>
-    		<tr>
-    			<th>Orb of Transmutation</th>
-    			{this.addTD("OrbofTransmutation", 13).map((ratio) => <td>{ratio}</td>)}
-    		</tr>
-    		<tr>
-    			<th>Regal Orb</th>
-    			{this.addTD("RegalOrb", 14).map((ratio) => <td>{ratio}</td>)}
-    		</tr>
-    		<tr>
-    			<th>Vaal Orb</th>
-    			{this.addTD("VaalOrb", 15).map((ratio) => <td>{ratio}</td>)}
-    		</tr>
-    		<tr>
-    			<th>Armourer's Scrap</th>
-    			{this.addTD("ArmourerScrap", 16).map((ratio) => <td>{ratio}</td>)}
-    		</tr>
+                <th>Armourer's Scrap</th>
+                {this.addTD("ArmourerScrap", 0).map((ratio, index) => <td key={index}>{ratio}</td>)}
+            </tr>
     		<tr>
     			<th>Blacksmith's Whetstone</th>
-    			{this.addTD("BlacksmithWhetstone", 17).map((ratio) => <td>{ratio}</td>)}
+    			{this.addTD("BlacksmithWhetstone", 1).map((ratio, index) => <td key={index}>{ratio}</td>)}
     		</tr>
+            <tr>
+                <th>Blessed Orb</th>
+                {this.addTD("BlessedOrb", 2).map((ratio, index) => <td key={index}>{ratio}</td>)}
+            </tr>
     		<tr>
     			<th>Cartographer's Chisel</th>
-    			{this.addTD("CartographerChisel", 18).map((ratio) => <td>{ratio}</td>)}
+    			{this.addTD("CartographerChisel", 3).map((ratio, index) => <td key={index}>{ratio}</td>)}
     		</tr>
-    		<tr>
-    			<th>Gemcutter's Prism</th>
-    			{this.addTD("GemcutterPrism", 19).map((ratio) => <td>{ratio}</td>)}
-    		</tr>
+            <tr>
+                <th>Chaos Orb</th>
+                {this.addTD("ChaosOrb", 4).map((ratio, index) => <td key={index}>{ratio}</td>)}
+            </tr>
+            <tr>
+                <th>Chromatic Orb</th>
+                {this.addTD("ChromaticOrb", 5).map((ratio, index) => <td key={index}>{ratio}</td>)}
+            </tr>
+            <tr>
+                <th>Divine Orb</th>
+                {this.addTD("DivineOrb", 6).map((ratio, index) => <td key={index}>{ratio}</td>)}
+            </tr>
+            <tr>
+                <th>Exalted Orb</th>
+                {this.addTD("ExaltedOrb", 7).map((ratio, index) => <td key={index}>{ratio}</td>)}
+            </tr>
+            <tr>
+                <th>Gemcutter's Prism</th>
+                {this.addTD("GemcutterPrism", 8).map((ratio, index) => <td key={index}>{ratio}</td>)}
+            </tr>
     		<tr>
     			<th>Glassblower's Bauble</th>
-    			{this.addTD("GlassblowerBubble", 20).map((ratio) => <td>{ratio}</td>)}
+    			{this.addTD("GlassblowerBubble", 9).map((ratio, index) => <td key={index}>{ratio}</td>)}
     		</tr>
+            <tr>
+                <th>Jeweller's Orb</th>
+                {this.addTD("JewellerOrb", 10).map((ratio, index) => <td key={index}>{ratio}</td>)}
+            </tr>
+            <tr>
+                <th>Orb of Alchemy</th>
+                {this.addTD("OrbofAlchemy", 11).map((ratio, index) => <td key={index}>{ratio}</td>)}
+            </tr>
+            <tr>
+                <th>Orb of Alteration</th>
+                {this.addTD("OrbofAlteration", 12).map((ratio, index) => <td key={index}>{ratio}</td>)}
+            </tr>
+            <tr>
+                <th>Orb of Augmentation</th>
+                {this.addTD("OrbofAugmentation", 13).map((ratio, index) => <td key={index}>{ratio}</td>)}
+            </tr>
+            <tr>
+                <th>Orb of Chance</th>
+                {this.addTD("OrbofChance", 14).map((ratio, index) => <td key={index}>{ratio}</td>)}
+            </tr>
+            <tr>
+                <th>Orb of Fusing</th>
+                {this.addTD("OrbofFusing", 15).map((ratio, index) => <td key={index}>{ratio}</td>)}
+            </tr>
+            <tr>
+                <th>Orb of Regret</th>
+                {this.addTD("OrbofRegret", 16).map((ratio, index) => <td key={index}>{ratio}</td>)}
+            </tr>
+            <tr>
+                <th>Orb of Scouring</th>
+                {this.addTD("OrbofScouring", 17).map((ratio, index) => <td key={index}>{ratio}</td>)}
+            </tr>
+            <tr>
+                <th>Orb of Transmutation</th>
+                {this.addTD("OrbofTransmutation", 18).map((ratio, index) => <td key={index}>{ratio}</td>)}
+            </tr>
     		<tr>
     			<th>Portal Scroll</th>
-    			{this.addTD("PortalScroll", 21).map((ratio) => <td>{ratio}</td>)}
+    			{this.addTD("PortalScroll", 19).map((ratio, index) => <td key={index}>{ratio}</td>)}
     		</tr>
+            <tr>
+                <th>Regal Orb</th>
+                {this.addTD("RegalOrb", 20).map((ratio, index) => <td key={index}>{ratio}</td>)}
+            </tr>
     		<tr>
     			<th>Scroll of Wisdom</th>
-    			{this.addTD("ScrollofWisom", 22).map((ratio) => <td>{ratio}</td>)}
+    			{this.addTD("ScrollofWisdom", 21).map((ratio, index) => <td key={index}>{ratio}</td>)}
     		</tr>
+            <tr>
+                <th>Vaal Orb</th>
+                {this.addTD("VaalOrb", 22).map((ratio, index) => <td key={index}>{ratio}</td>)}
+            </tr>
             </tbody>
     			
     		</table>
