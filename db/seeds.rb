d24fe0baac18662158f6e0646e20630050f7cfd8
ChesterHub <ChesterLHoang@gmail.com>
2017-08-10 23:23:07 -0700

    New league: harbinger

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@ League.create(league_type: "Breach")
 League.create(league_type: "Hardcore+Breach")
 League.create(league_type: "Legacy")
 League.create(league_type: "Hardcore+Legacy")
+League.create(league_type: "Harbinger")
+League.create(league_type: "Hardcore+Harbinger")
 
 # 30.times do
 # 	ChaosOrb.create(league_id: rand(1..4), ign: Faker::Name.first_name, blessed_orb: rand(0.1..50.0), cartographer_chisel: rand(0.1..50.0), chromatic_orb: rand(0.1..50.0), divine_orb: rand(0.1..50.0), eternal_orb: rand(0.1..50.0), exalted_orb: rand(0.1..50.0), gemcutter_prism: rand(0.1..50.0), jeweller_orb: rand(0.1..50.0), orbof_alchemy: rand(0.1..50.0), orbof_alteration: rand(0.1..50.0), orbof_chance: rand(0.1..50.0), orbof_fusing: rand(0.1..50.0), orbof_regret: rand(0.1..50.0), orbof_scouring: rand(0.1..50.0), regal_orb: rand(0.1..50.0), vaal_orb: rand(0.1..50.0), scrollof_wisdom: rand(0.1..50.0), portal_scroll: rand(0.1..50.0), armourer_scrap: rand(0.1..50.0), blacksmith_whetstone: rand(0.1..50.0), glassblower_bubble: rand(0.1..50.0), orbof_transmutation: rand(0.1..50.0), orbof_augmentation: rand(0.1..50.0))
